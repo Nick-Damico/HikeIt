033a68978d87f156492619cdce5f1b6e8bc8d44f
Nicholas D'Amico <nickalan82@icloud.com>
2017-11-07 12:07:50 -0500

    Added validation for notes maximum length of 200 characters

--- app/models/hike.rb
+++ app/models/hike.rb
@@ -1,4 +1,5 @@
 class Hike < ApplicationRecord
 	validates :title, :description, :presence => true
 	validates :description, :length => { :maximum => 390 }
+	validates :notes, :length => { :maximum => 200 }
 end
