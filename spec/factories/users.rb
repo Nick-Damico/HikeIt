d15274f597c9ce90b7e742e632c35466c8d411d9
Nicholas D'Amico <nickalan82@icloud.com>
2017-11-06 22:19:47 -0500

    rails generated devise model for user, added custom column for trail_name, bio, profile_image to db users table

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+require 'Faker'
+FactoryBot.define do
+  factory :user do
+  	# Faker::Omniauth.facebook
+  	trail_name {Faker::Pokemon.name}
+    email {Faker::Internet.email}
+    password {}
+
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -27,7 +27,7 @@ require 'capybara/rspec'
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
-Capybara.javascript_driver :webkit
+# Capybara.javascript_driver :webkit # Possible Deprecated after Capybara-webkit
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
