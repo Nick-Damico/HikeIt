f9ddd98c37b7b279bf0ffab2225fee448cc67d2e
Nicholas D'Amico <nickalan82@icloud.com>
2017-11-07 14:11:45 -0500

    defined a valid factory for testing, defined initail tests for validating a hiking_trail

---
+++ spec/factories/hiking_trails.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :hiking_trail do
+    name "Alum Cave Trail"
+    location "Newfound Gap Road"
+    area "Gatlinburg / Mt. LeConte"
+    distance 4.4
+    elevation_gain 1125
+    difficulty_rating 7
+  end
+end
---
+++ spec/models/hiking_trail_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe HikingTrail, type: :model do
+  it 'is valid with a name, location, area, distance'
+
+  it 'is invalid without a name'
+
+  it 'is invalid without a location'
+
+  it 'is invalid without a area'
+
+  it 'is invalid without a distance'
+
+  it 'is valid with a unique name'
+
+end
