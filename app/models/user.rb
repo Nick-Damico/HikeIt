6936a56cc16e0d68d9ab0379adbd22a5142a5c2f
Nicholas D'Amico <nickalan82@icloud.com>
2017-11-07 11:11:42 -0500

    Added validations for trail_name, bio, location

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+	validates :trail_name, :uniqueness => true
+	validates :bio, length: { maximum: 160 }
+	validates :location, length: { maximum: 21 }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
