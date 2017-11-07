9b05df447a7c1d261445dfba7c9823f8bed0c99f
Nicholas D'Amico <nickalan82@icloud.com>
2017-11-07 12:38:30 -0500

    Added custom validation to pass spec test on valid hike_date which cannot be a date or time in the past

--- app/models/hike.rb
+++ app/models/hike.rb
@@ -2,4 +2,15 @@ class Hike < ApplicationRecord
 	validates :title, :description, :presence => true
 	validates :description, :length => { :maximum => 390 }
 	validates :notes, :length => { :maximum => 200 }
+	validate :valid_hike_date
+	# Will need future validations and test to check for belong_to :user(leader),
+		# Also test for belong_to relationship with hiking_trail
+
+	private 
+
+	def valid_hike_date		
+		if hike_date < Time.now
+			errors.add(:hike_date, "date can't be in the past")
+		end
+	end
 end
