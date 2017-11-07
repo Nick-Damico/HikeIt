4f9b84d3cc4680afbc6bd5661608c5e1113e0aa7
Nicholas D'Amico <nickalan82@icloud.com>
2017-11-07 11:25:03 -0500

    Devise User defined, deleted hiking-trails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107025228) do
+ActiveRecord::Schema.define(version: 20171107150053) do
 
   create_table "users", force: :cascade do |t|
     t.string "trail_name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20171107025228) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "location"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
