35771c63f069f19ece52a7d5a82be0a2b0d6a6fe
Nicholas D'Amico <nickalan82@icloud.com>
2017-11-07 11:21:48 -0500

    finished spec tests for users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,23 +2,64 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   
-  it "has a valid User factory with an email and password" do
-  	expect(build(:user)).to be_valid
-  end
+	it "has a valid User factory with an email and password" do
+		expect(build(:user)).to be_valid
+	end
 
-  it "is invalid without an email" do
-  	expect(build(:user, email: nil)).to_not be_valid
-  end
+	it "is invalid without an email" do
+		expect(build(:user, email: nil)).to_not be_valid
+	end
 
-  it "is invalid without a valid email (ex. user@example.com)" do
-  	expect(build(:user, email: "happyhikerATexample.com")).to_not be_valid
-  end
+	it "is invalid without a valid email (ex. user@example.com)" do
+		user = build(:user)
+		expect(build(:user, email: "#{user.trail_name}.example.com")).to_not be_valid
+	end
 
-  it "is invalid without a password"
+	describe "password" do
+	  it "is invalid without a password" do
+	  	expect(build(:user, password: nil)).to_not be_valid
+	  end
 
-  it "is invalid without a password length between 8 - 20 characters"
+	  describe "when password length is invalid" do
 
-  it "encrypts the password on persisting the user to database"
+	  	it "is invalid with a password length < 8 characters" do
+	  		expect(build(:user, password: "happy")).to_not be_valid
+	  	end	
 
+	  	it "is invalid with a password length > 20 characters" do
+	  		expect(build(:user, password: "123456789101112131411")).to_not be_valid
+	  	end
+
+	  end
+	  
+
+	  it "encrypts the password on persisting the user to database" do
+	  	user = build(:user)
+	  	user.save
+	  	expect(user.encrypted_password).to_not eq(user.password)
+	  end
+
+end
+
+	it "is invalid with trail_name that is not unique" do
+		user_1 = build(:user)
+		user_1.save
+		user_2 = build(:user, trail_name: "#{user_1.trail_name}", email: "newuser@example.com")
+		expect(user_2).to_not be_valid
+	end
+
+	it "is invalid with a bio length greater than 160 characters" do
+		user = build(:user,
+					 bio: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right a"
+			)
+		expect(user).to_not be_valid
+	end
+
+	it "is invalid with a location length greater than 21 characters" do
+		user = build(:user, 
+			location: "Winchester-on-the-Severn1, Maryland"
+			)
+		expect(user).to_not be_valid
+	end
 
 end
