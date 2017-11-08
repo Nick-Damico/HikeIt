54309375d930d8537d7587f5e8cbb7c4c3ce7259
Nicholas D'Amico <nickalan82@icloud.com>
2017-11-08 13:26:40 -0500

    macro belongs_to :feature added

--- app/models/hiking_trail.rb
+++ app/models/hiking_trail.rb
@@ -2,4 +2,5 @@ class HikingTrail < ApplicationRecord
 	validates :name, :location, :area, :distance, :presence => true
 	validates :name, :uniqueness => true
 	has_many :hikes
+	belongs_to :feature
 end
