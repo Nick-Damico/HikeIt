6a9d6a87d3f7c439264016478a843a2f81057928
Nicholas D'Amico <nickalan82@icloud.com>
2017-11-08 13:19:21 -0500

    defined factory for Feature, built test for valid with :title, test added for has_many association with hiking_trail

--- spec/factories/features.rb
+++ spec/factories/features.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :feature do
-    title "MyString"
+    title "Waterfalls"
   end
 end
--- spec/models/feature_spec.rb
+++ spec/models/feature_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Feature, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory and valid with title' do
+  	expect(build(:feature)).to be_valid
+  end
+
+  it 'has many hiking trails' do 
+  	@feature = build(:feature)
+  	@hiking_trail = build(:hiking_trail)
+    @feature.hiking_trails << @hiking_trail
+    expect(@feature.hiking_trails).to include(@hiking_trail)
+  end
+  
 end
