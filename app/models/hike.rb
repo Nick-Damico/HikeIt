67226d62bde482eae57ec9bd01e3b282b18310af
Nicholas D'Amico <nickalan82@icloud.com>
2017-11-08 10:06:19 -0500

    lifecycle callback :before_save #format_date added to add a more human readable data to database

--- app/models/hike.rb
+++ app/models/hike.rb
@@ -1,14 +1,18 @@
 class Hike < ApplicationRecord
+	# Associations
 	has_many :planned_hikes
 	has_many :users, through: :planned_hikes
 	belongs_to :leader, :class_name => "User", :foreign_key => 'leader_id'
-
+	# Validations
 	validates :title, :description, :hike_date, :presence => true
 	validates :description, :length => { :maximum => 390 }
 	validates :notes, :length => { :maximum => 200 }
 	validate :valid_hike_date
 	# Will need future validations and test to check for belong_to :user(leader),
 		# Also test for belong_to relationship with hiking_trail
+	# Lifecycle callbacks
+	before_save :format_date
+	
 
 	private 
 
@@ -16,5 +20,9 @@ class Hike < ApplicationRecord
 		if !hike_date.nil? && hike_date < Time.now
 			errors.add(:hike_date, "date can't be in the past")
 		end
+	end
+
+	def format_date
+		self.hike_date = hike_date.strftime("%B %d, %Y")
 	end
 end
