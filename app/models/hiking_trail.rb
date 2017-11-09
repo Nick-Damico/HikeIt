182a95a3d1f1e857214aecc7a3f7532ccbf52eea
Nicholas D'Amico <nickalan82@icloud.com>
2017-11-09 10:04:39 -0500

    constant TRAIL_AREAS with an area of existing hiking trail ares in the GSMNP, function is to provide scraper with information when building instances from table data on 'hikingthesmokies.com'

--- app/models/hiking_trail.rb
+++ app/models/hiking_trail.rb
@@ -3,4 +3,14 @@ class HikingTrail < ApplicationRecord
 	validates :name, :uniqueness => true
 	has_many :hikes
 	belongs_to :feature
+
+	TRAIL_AREAS = [
+		"Gatlinburg / Mt. LeConte Area",
+		"Newfound Gap / Clingmans Dome Area",
+		"Cades Cove / Townsend Area",
+		"Elkmont Area",
+		"Cherokee / Deep Creek Area",
+		"Greenbrier / Cosby Area",
+		"Balsam Mountain / Big Creek / Cataloochee / Fontana Lake"
+	]
 end
---
+++ app/services/scraper_service.rb
@@ -0,0 +1,15 @@
+class ScraperService 
+
+	attr_accessor :url
+
+	def initialize(args*)	
+		binding.pry
+		
+	end
+
+	def self.scrape_hiking
+		
+	end
+end
+
+# scrapperScrapperService.new("http://www.hikinginthesmokys.com/")
