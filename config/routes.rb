7f927a75371513b3c7fbbaca31b015542c50ac83
Nicholas D'Amico <nickalan82@icloud.com>
2017-11-09 21:48:09 -0500

    RESTful resources macro added hiking_trails

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
-# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	resources :hiking_trails
+	devise_for :users
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-# StaticController
-  root 'static#home'
+	# StaticController
+	root 'static#home'
 
-  get 'static/home'
+	get 'static/home'
 
-  get 'static/about'
+	get 'static/about'
 
-  get 'static/contact'
+	get 'static/contact'
 
-# HikeController
+	# HikeController
 
 end
