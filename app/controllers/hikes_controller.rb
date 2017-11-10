ce5145898cd7a5a1ff05a5a402d178534dd17665
Nicholas D'Amico <nickalan82@icloud.com>
2017-11-10 14:45:46 -0500

    action for show, create added/built, private method hike_params defined with whitelisted param hash keys for instantiating a new instance of Hike class

--- app/controllers/hikes_controller.rb
+++ app/controllers/hikes_controller.rb
@@ -7,5 +7,31 @@ class HikesController < ApplicationController
 	def new
 		@hike = Hike.new
 	end
+
+	def show
+		@hike = Hike.find_by(id: params[:id])
+	end
 	
+	def create
+		@hike = Hike.new(hike_params)
+		if @hike.save
+			redirect_to hike_path(@hike.id)
+		else
+			render :new
+		end
+	end
+
+	private	
+		def hike_params
+			params.require(:hike).permit(
+					:title,
+					:description,
+					:hike_date,
+					:hike_time,
+					:notes,
+					:leader_id,
+					:hiking_trail_id
+				)
+		end
+		
 end
