999b4670d9c46f1e5b5aaeea0cc64a979f13dc9f
Nicholas D'Amico <nickalan82@icloud.com>
2017-11-10 20:51:09 -0500

    actions added for create, instance variable in views show, new defined. built out full action loop for create and defined private method .hiking_trail_params for whitelisting parameters

--- app/controllers/hiking_trails_controller.rb
+++ app/controllers/hiking_trails_controller.rb
@@ -4,11 +4,35 @@ class HikingTrailsController < ApplicationController
   end
 
   def show
+    @hiking_trail = HikingTrail.find_by(id: params[:id])
   end
 
   def edit
   end
 
   def new
+  	@hiking_trail = HikingTrail.new
+  end
+
+  def create
+  	@hiking_trail = HikingTrail.new(hiking_trail_params)
+    if @hiking_trail.save
+      redirect_to hiking_trail_url(@hiking_trail)
+    else
+      render :new
+    end
+  end
+
+  private
+  def hiking_trail_params
+  	params.require(:hiking_trail).permit(
+  			:name,
+  			:location,
+  			:area,
+  			:distance,
+  			:elevation_gain,
+  			:difficulty_rating,
+  			:feature_id
+  		)
   end
 end
