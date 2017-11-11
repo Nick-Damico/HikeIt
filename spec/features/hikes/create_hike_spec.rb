33cf2dc48664aa15d9c097adc5ec659f21fed727
Nicholas D'Amico <nickalan82@icloud.com>
2017-11-10 20:58:17 -0500

    started adding feature tests

--- spec/features/hikes/create_hike_spec.rb
+++ spec/features/hikes/create_hike_spec.rb
@@ -1,6 +1,7 @@
-require 'rspec_helper'
+require 'rails_helper'
 
 RSpec.feature 'Add a Hike', :type => :feature do 
+
 	scenario "User is able to Create a Planned Hike" do
 		#1. go to the root path a find a button to create a hike
 		visit '/'
@@ -11,12 +12,18 @@ RSpec.feature 'Add a Hike', :type => :feature do
 		#4. Fill in a brief description of the planned hike event
 		fill_in 'hike_description', with: "Our Weekly Group Hike to a new and fun trail"
 		#5. Select from a datetime select the Date for a hike.
-
+		select "hike_hike_date", :from => "organizationSelect"
 		#6. Select from a datetime select the Time of the hike.
+		select "hike_hike_time", :from => ""
 		#7. Select A hiking-trail for this planned hike event.
+		select "hike_hiking_trail", :from => ""
 		#8.	Fill in any notes for the planned hike.
+		fill_in 'hike_notes', with: "It's going to be chilly up there this time of year, bring warm clothing."
 		#9. Click 'Create Hike' submit button.
+		click_link 'Create Hike'
 		#10. Redirect to the hike show page with details of the planned Hike
 			 # 10.1. with ( title, description, hike_date, hike_time, notes, hiking_trail.name )
+			 expect(page).to have_content('Weekly Sunday Morning Hike')
+			 expect(page).to have_content('Our Weekly Group Hike to a new and fun trail')
 	end
 end
