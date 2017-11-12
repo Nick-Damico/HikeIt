396bbb8372d3e84b121e655ed52d8a14a6ba0d09
Nicholas D'Amico <nickalan82@icloud.com>
2017-11-12 13:39:00 -0500

    custom route drawn for join route, adds current_user to existing hike

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 	resources :hiking_trails
 	resources :hikes
+
+	post 'hikes/:id/join', :to => 'hikes#join', :as => 'join'
 	devise_for :users
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
