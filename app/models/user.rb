e3f0580c256ea6c2da7b3794b9671938a66befd8
Nicholas D'Amico <nickalan82@icloud.com>
2017-11-13 10:22:54 -0500

    #planned_hikes query defined

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  def planned_hikes
+  	Hike.all.where(leader_id: self.id)
+  end
+  
 end
