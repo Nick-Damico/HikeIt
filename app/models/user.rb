4c7de8bbe368b85b6c13700d6a9bdcef9665f6cb
Nicholas D'Amico <nickalan82@icloud.com>
2017-11-20 12:14:10 -0500

    error fixed due to comma on block creation block

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,8 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
+      user.email = auth.info.email, 
+      user.profile_image = auth.info.image,
       user.password = Devise.friendly_token[0,20]
     end      
   end
