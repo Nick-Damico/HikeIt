cf419537209d74536f4031f5223142c0053ea273
Nicholas D'Amico <nickalan82@icloud.com>
2017-11-19 22:45:43 -0500

    redirect on successful login with omniauth provider directs User to static/home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 	layout :layout_by_resource
 
   	def after_sign_in_path_for(resource)
-  		request.env['omniauth.origin'] || static_home_path
+  		static_home_path || request.env['omniauth.origin'] 
 	end
 
 	private
