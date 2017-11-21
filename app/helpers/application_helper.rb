06e8e5e4ddaf9c9402156b25b694ac91ab243d31
Nicholas D'Amico <nickalan82@icloud.com>
2017-11-21 14:50:21 -0500

    #logo_path logic moved from partial to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,5 +3,14 @@ module ApplicationHelper
 	def title(text)
 		content_for :title, "HikeIt | #{text}"
 	end
+
+	def logo_path
+		if signed_in?
+			link_to "HikeIt", static_home_path, :class => "text-white logo-text btn-outline-light btn"
+		else
+			link_to "HikeIt", root_path, :class => "text-white logo-text btn-outline-light btn"
+		end
+
+	end
 	
 end
