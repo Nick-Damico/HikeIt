a881bb449304dd1a58d06ebea8a9ee397193cae8
Nicholas D'Amico <nickalan82@icloud.com>
2017-11-21 12:30:49 -0500

    two new helpers #trail_name, #trail_area returns instance value, added to avoid multiple string together multiple methods

--- app/helpers/hiking_trails_helper.rb
+++ app/helpers/hiking_trails_helper.rb
@@ -1,2 +1,24 @@
 module HikingTrailsHelper
+
+	def trail_image_tag(trail, view)		
+		if trail.image_url
+			if view == 'index'
+				image_tag "#{trail.image_url}", :class => "img-fluid w-100 card-img" 
+			elsif view == 'show'
+				image_tag "#{trail.image_url}", :class => 'img-thumbnail'
+			end
+		else
+			image_tag "btn-find-hike", :options => { :size => "280x200" }, :class => "img-fluid card-img w-100"
+		end
+	end
+
+	def trail_name(trail)
+		trail.name
+	end
+
+	def trail_area(trail)
+		trail.area
+	end
+
+
 end
