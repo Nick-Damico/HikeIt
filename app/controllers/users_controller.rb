336bb6a3947955d209e41f2ebe29a7b407035d8d
Nicholas D'Amico <nickalan82@icloud.com>
2017-11-21 09:41:42 -0500

    logic added to Users_controller for :show, :edit, :update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,33 @@ class UsersController < ApplicationController
 	before_action :authenticate_user!
 
   def show
-	  @user = User.find(params[:id])
+  	@user = User.find_by(id: params[:id])
+  	if @user.nil?
+  		redirect_to hikes_path
+  	end
+  end
+
+  def edit
+  	@user = User.find_by(id: params[:id])
+  end
+
+  def update
+  	@user = User.find_by(id: params[:id])
+  	if @user.update(user_params)
+  		redirect_to user_path(@user)
+  	else
+  		render :show
+  	end
+
+  end
+
+
+  private 
+  def user_params
+  	params.require(:user).permit(
+  		:trail_name,
+  		:bio,
+  		:profile_image
+  		)
   end
 end
