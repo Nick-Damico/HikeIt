e701192b9d35e6f7218e91474138b205fc7f999e
Nicholas D'Amico <nickalan82@icloud.com>
2017-11-22 15:16:47 -0500

    #display_profile_img, #hikes_joined helper methods created

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,15 @@
 module UsersHelper
+
+	def display_profile_img(user) 
+		if user.profile_image.nil? || user.profile_image.empty?
+			image_tag "profile-temp.svg", :class => "profile_pic rounded-circle img-thumbnail"
+	 	else 
+			image_tag "#{user.profile_image}", :class => "profile_pic rounded-circle img-thumbnail" 
+		end 
+	end
+
+	def hikes_joined(user,hikes)
+		@hikes = hikes.select { |hike| hike if hike.leader != user }
+	end
+
 end
