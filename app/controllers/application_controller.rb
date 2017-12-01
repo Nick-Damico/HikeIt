c2561eac2f2121d94a6645315a2f24aee05eeb6f
Nicholas D'Amico <nickalan82@icloud.com>
2017-12-01 17:29:05 -0500

    before_action :redirect_to_home added so a signed in User cannot access the landing page of the app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception  
 	layout :layout_by_resource
+	before_action :redirect_to_home
 
   	def after_sign_in_path_for(resource)
   		home_path || request.env['omniauth.origin'] 
@@ -14,6 +15,10 @@ class ApplicationController < ActionController::Base
 		else
 		  "application"
 		end
+	end
+
+	def redirect_to_home
+		redirect_to home_url if params[:action] == 'landing' && current_user
 	end
 
 end
