a490eb879ce80631912c5a66f0ef0a605b8ee423
Nicholas D'Amico <nickalan82@icloud.com>
2017-12-01 13:12:50 -0500

    Select filter added to :index view. Logic for filter all Trails in databse by :distance, :difficulty_rating, :area handled by new methods .getTrails, .get_trails_by_difficulty, .check_difficulty, additional scoped methods for filter added :difficulty_easy(_medium, _strenous) :distance_short(_medium,_long,_extreme)

--- app/models/hiking_trail.rb
+++ app/models/hiking_trail.rb
@@ -8,6 +8,15 @@ class HikingTrail < ApplicationRecord
 	before_validation :rating_to_integer
 
 	scope :by_area, -> (trail_area) { where('area = ?', trail_area) }
+
+	scope :difficulty_easy, -> { where('difficulty_rating < ?',5) }
+	scope :difficulty_moderate, -> { where('difficulty_rating > ? AND difficulty_rating < ?', 4,10) }
+	scope :difficulty_strenous, -> { where('difficulty_rating > ?', 10) }
+
+	scope :distance_short, -> { where('distance < ?',5) }
+	scope :distance_medium, -> { where('distance > ? AND distance < ?',4,10) }
+	scope :distance_long, -> { where('distance > ? AND distance < ?',9, 16) }
+	scope :distance_extreme, -> { where('distance > ?',15) }
 	
 	TRAIL_AREAS = [
 		"Gatlinburg / Mt. LeConte Area",
@@ -18,6 +27,8 @@ class HikingTrail < ApplicationRecord
 		"Greenbrier / Cosby Area",
 		"Balsam Mountain / Big Creek / Cataloochee / Fontana Lake"
 	]
+
+	TRAIL_LENGTH = ['easy', 'short', 'medium', 'long', 'extreme']
 
 	def rating_to_integer
 		if self.difficulty_rating.class == Float
@@ -33,6 +44,35 @@ class HikingTrail < ApplicationRecord
 				self.feature_id = feature.id
 			end
 		end		
+	end
+
+	def self.check_difficulty(attribute)
+		attribute == 'easy' || attribute == 'moderate' || attribute == 'strenous'
+	end
+
+	def self.get_trails_by_difficulty(rating)
+		self.send("difficulty_#{rating}").order(:difficulty_rating)
+	end
+
+	def self.get_trails_by_distance(length)
+		self.send("distance_#{length}").order(:distance)
+	end
+
+	def self.getTrails(params)		
+		if !params[:attribute].blank? 
+			attribute_value = params[:attribute]
+			if TRAIL_AREAS.include?(attribute_value)
+		    	self.by_area(attribute_value)
+		    elsif check_difficulty(attribute_value)
+		    	self.get_trails_by_difficulty(attribute_value)
+		    elsif TRAIL_LENGTH.include?(attribute_value)
+		    	self.get_trails_by_distance(attribute_value)
+		    else  
+		      	self.all
+		    end 
+		else
+			self.all    	
+		end
 	end
 
 end
