96fcf7f2f37fc56c8c82d6c8f8cd759cbd8a566a
Nicholas D'Amico <nickalan82@icloud.com>
2017-12-04 15:35:33 -0500

    defined :create action for hikes nested route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   	devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   	resources :users do
-  		resources :hikes, only: [:index, :show, :new]
+  		resources :hikes, only: [:index, :show, :new, :create]
   	end
 
 	resources :hiking_trails
