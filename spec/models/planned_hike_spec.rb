41e300b8cb0a7389eaf96be437659a8d087b22bb
Nicholas D'Amico <nickalan82@icloud.com>
2017-12-06 08:29:54 -0500

    fixing outdated tests

--- spec/models/planned_hike_spec.rb
+++ spec/models/planned_hike_spec.rb
@@ -3,9 +3,11 @@ require 'rails_helper'
 RSpec.describe PlannedHike, type: :model do
 
 	before  do
+		@feature = Feature.create!(title: "Caves")
+		@hiking_trail = HikingTrail.create!(name: 'Alum Cave Trail', location: "GSMNP", area: "Gatlinburg", distance: 5.5, feature_id: @feature.id)
 		@user = User.create(trail_name: 'Dreadful Rebel', email: "d_rebel@hikesmokies.com", password: "happyhiker20")		
-		@hike = Hike.create!(title: "Sunday Night Hike", description: "Our bi-weekly Sunday night hike will be to Chimmney Tops Trail.", hike_date: Time.new(2017,12,20), leader_id: @user.id)
-		@planned_hike = PlannedHike.create!(user_id: @user.id, hike_id: @hike.id)
+		@hike = Hike.create!(title: "Sunday Night Hike", description: "Our bi-weekly Sunday night hike will be to Chimmney Tops Trail.",hiking_trail_id: @hiking_trail.id, hike_date: Time.new(2017,12,20), leader_id: @user.id)
+		@planned_hike = PlannedHike.create!(user_id: @user.id, hike_id: @hike.id, hike_type: 0)
 	end
 
 	it 'belongs to a user' do
@@ -14,9 +16,5 @@ RSpec.describe PlannedHike, type: :model do
 
 	it 'belongs to a hike' do
 		expect(@planned_hike.hike).to eq(@hike)
-	end
-
-	it 'has a date through hike' do
-		expect(@planned_hike.hike.hike_date).to eq("December 20, 2017")
 	end
 end
