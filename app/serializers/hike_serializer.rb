aa791d84f95c71ae26a3799ffa256cbe6da06541
Nicholas D'Amico <nickalan82@icloud.com>
2018-01-07 11:15:20 -0500

    added has_many relationship :planned_hikes that uses PlannedHikeSerializer to render info

--- app/serializers/hike_serializer.rb
+++ app/serializers/hike_serializer.rb
@@ -1,4 +1,5 @@
 class HikeSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :hike_date
   belongs_to :hiking_trail, serializer: HikingTrailSerializer
+  has_many :planned_hikes, serializer: PlannedHikeSerializer
 end
