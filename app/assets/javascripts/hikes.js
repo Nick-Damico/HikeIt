a53c1e238a368cbb1375c317de16c88b67c5248d
Nicholas D'Amico <nickalan82@icloud.com>
2018-01-07 20:20:08 -0500

    added function constructor class Hike, function buildHikes(data) takes server returned JSON data and creates instances of Hike to utilize with Handlebars.js templates, completed $.ajax request to /hikes

--- app/assets/javascripts/hikes.js
+++ app/assets/javascripts/hikes.js
@@ -1,9 +1,52 @@
 //////////////////////////////////////////
+//  Function Prototypes
+//////////////////////////////////////////
+
+function Hike(data) {
+  this.id = data.id;
+  this.title = data.title;
+  this.description = data.description;
+  this.hike_date = data.hike_date;
+  this.trail = data.hiking_trail.name;
+  this.trail_image = data.hiking_trail.image_url;
+  this.hike_type = data.planned_hikes[0].hike_type;
+}
+
+Hike.prototype.truncateText = function() {
+  this.description = this.description.slice(0,45
+  );
+  this.description += '...';
+}
+
+Hike.prototype.formatDate = function() {
+  let date = new Date(this.hike_date);
+  this.date_formatted = date.toString().split(' ').slice(1, 4).join(' ');
+}
+
+
+
+//////////////////////////////////////////
 //  Global Functions
 //////////////////////////////////////////
 
 function attachListeners() {
   findHikeListener();
+}
+
+function buildHikes(data) {
+  let hikes = [];
+  for (let item of data) {
+    let hike = new Hike(item);
+    hikes.push(hike);
+  }
+  return hikes;
+}
+
+function formatForOutput(hikes) {
+  for (let hike of hikes) {
+    hike.truncateText();
+    hike.formatDate();
+  }
 }
 
 
@@ -16,9 +59,14 @@ function findHikeListener() {
   $('#findHikeBtn').on('click', function(e) {
     // Stop default request 'GET', /hikes
     e.preventDefault();
-    getHikes(e.target);
+    $('#mainContent').fadeOut('slow', function() {
+      $(this).html("");
+      getHikes(e.target);
+    });
+
   });
 }
+
 
 
 //////////////////////////////////////////
@@ -35,8 +83,11 @@ function getHikes(target) {
     url: formUrl,
     dataType: 'json'
   }).done(function(data) {
-    // Get response
-    // load data into DOM
+    let hikes = buildHikes(data);
+    formatForOutput(hikes);
+    let html = HandlebarsTemplates['hikes/index']({ hike: hikes })
+    $('#mainContent').html(html).fadeIn('slow');
+
   }).fail(function() {
     alert('error')
   });
