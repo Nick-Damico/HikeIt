dc90631446f8a897543eecd77c1ece6c52098c92
Nicholas D'Amico <nickalan82@icloud.com>
2018-01-09 07:37:18 -0500

    user serializer added, hike has_many relationship with :users added to return User information

--- app/serializers/hike_serializer.rb
+++ app/serializers/hike_serializer.rb
@@ -2,4 +2,5 @@ class HikeSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :hike_date, :hike_time, :notes
   belongs_to :hiking_trail, serializer: HikingTrailSerializer
   has_many :planned_hikes, serializer: PlannedHikeSerializer
+  has_many :users, serializer: UserSerializer
 end
