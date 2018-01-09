aeb0dc0c532e6bb4ce70c3da697f7e3ccbd9fefd
Nicholas D'Amico <nickalan82@icloud.com>
2018-01-09 07:29:11 -0500

    added additional properties of hike constructor that get added during initialization of an object

--- app/assets/javascripts/oop.js
+++ app/assets/javascripts/oop.js
@@ -7,10 +7,14 @@ function Hike(data) {
   this.id = data.id;
   this.title = data.title;
   this.description = data.description;
-  this.hike_date = moment(data.hike_date).format("MMM D, YYYY")
+  this.notes = data.notes;
+  this.date = moment(data.hike_date).format("MMM D, YYYY");
+  this.time = moment(data.hike_time).format("h:mm:ss a");
+  this.trail_id = data.hiking_trail.id;
   this.trail = data.hiking_trail.name;
   this.trail_image = data.hiking_trail.image_url;
   this.hike_type = data.planned_hikes[0].hike_type;
+  this.features = data.hiking_trail.feature.title;
   this.hikers = [];
   for (let hike of data.planned_hikes) {
     this.hikers.push(hike.user_id);
