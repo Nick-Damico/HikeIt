828894508ed3eaf4ff1c9bf04e2df1cc2a3b4cab
Nick-Damico <nickalan82@icloud.com>
2018-04-11 14:49:47 -0400

    add helper method display_more_info,'more info' button on show route of hiking trail page links to either the hiking trails detail page on 'hikingthesmokies.com' or, if value nil, defaults to 'hikingthesmokies.com' home page

--- app/helpers/hiking_trails_helper.rb
+++ app/helpers/hiking_trails_helper.rb
@@ -1,9 +1,9 @@
 module HikingTrailsHelper
 
-	def trail_image_tag(trail, view)		
+	def trail_image_tag(trail, view)
 		if trail.image_url
 			if view == 'index'
-				image_tag "#{trail.image_url}", :class => "img-fluid w-100 card-img" 
+				image_tag "#{trail.image_url}", :class => "img-fluid w-100 card-img"
 			elsif view == 'show'
 				image_tag "#{trail.image_url}", :class => 'img-thumbnail'
 			end
@@ -25,6 +25,11 @@ module HikingTrailsHelper
 			@content = content_tag(:h4, "Notes:", :class => "notes-header font-w-100 color-grey-red")
 			@content << content_tag(:p, "#{hike.notes}", :class => "notes-p")
 		end
+	end
+
+	def display_more_info(trail)
+		trail_url = trail.url ? trail.url : "http://www.hikinginthesmokys.com/"
+		button_to "More Info", trail_url, :class => "btn btn-color-sign_up text-white mt-3"
 	end
 
 
